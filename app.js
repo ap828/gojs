9fb43de35b816fb205824cb99e3bdf85f024e9f0
ap828 <abhishekpatel828@gmail.com>
2017-03-05 17:45:09 +0530

    session2

---
+++ app.js
@@ -0,0 +1,11 @@
+()();
+function{
+	'use strict';
+	var x=10;
+	angular.module("MyApp",[])
+	.controller("MyCtrl",function($scope){
+		$scope.hello={};
+		$scope.hello.title="AngularJs";
+	});
+}
+();
